9cb7649c592b0621ceddbb04cac86d78f478aeeb
Dilmurat Sarybaev <dilmuratsarybaev@gmail.com>
2023-10-09 08:07:51 +0600

    first commit

---
+++ Hubs/AuctionChat.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Auction.Hubs
+{
+    public class AuctionChat : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+
+            await Clients.All.SendAsync("RecieveMessage", user, message);
+        }
+    }
+}
+
---
+++ Models/UserMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Auction.Models
+{
+    public class UserMessage
+    {
+        public string Username { get; set; } = "";
+        public string Message { get; set; } = "";
+        public bool CurrentUser { get; set; }
+        public DateTime DateSend { get; set; }
+    }
+
+}
+
